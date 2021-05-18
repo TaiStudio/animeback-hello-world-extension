7630675e4f3a98dc707ad2223c88f68d3bb3c201
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-18 17:22:37 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+/*--------------------------------------------------------------------------------------\
+|  _______    _    _____ _             _ _           ________     ___   ___ ___  __     |
+| |__   __|  (_)  / ____| |           | (_)         /  ____  \   |__ \ / _ \__ \/_ |    |
+|    | | __ _ _  | (___ | |_ _   _  __| |_  ___    /  / ___|  \     ) | | | | ) || |    |
+|    | |/ _` | |  \___ \| __| | | |/ _` | |/ _ \  |  | |       |   / /| | | |/ / | |    |
+|    | | (_| | |  ____) | |_| |_| | (_| | | (_) | |  | |___    |  / /_| |_| / /_ | |    |
+|    |_|\__,_|_| |_____/ \__|\__,_|\__,_|_|\___/   \  \____|  /  |____|\___/____||_|    |
+|                                                   \________/                          |
+\--------------------------------------------------------------------------------------*/
+
+const extensionConfig = require('./extension.json');
+module.exports = {
+    main(ExtensionAPI) {
+        ExtensionAPI["ExtensionKit"].wallpaperExtend(`
+            <style>
+                .hello{
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    text-align: center;
+                    justify-content: center;
+                    flex-direction: column;
+                    font-size: 5em;
+                }
+            </style>
+            <div class="hello"><p>HELLO WORLD !</p></div>
+        `, extensionConfig.name);
+        return true;
+    }
+}
